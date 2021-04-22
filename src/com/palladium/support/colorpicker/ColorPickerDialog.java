Havoc-OS/android_vendor_support

support: Add VibrationUtils to reduce code redundancy
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/havoc/support/colorpicker/ColorPickerDialog.java
@@ -23,8 +23,6 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -36,6 +34,7 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 
 import com.havoc.support.R;
+import com.havoc.support.util.VibrationUtils;
 
 public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 
@@ -47,7 +46,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
     private OnColorChangedListener mListener;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public interface OnColorChangedListener {
         void onColorChanged(int color);
@@ -59,7 +57,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         init(initialColor);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     private void init(int color) {
@@ -156,7 +153,7 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
                 mListener.onColorChanged(mNewColor.getColor());
             }
         }
-        doHapticFeedback();
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
         dismiss();
     }
 
@@ -175,14 +172,5 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         super.onRestoreInstanceState(savedInstanceState);
         mOldColor.setColor(savedInstanceState.getInt("old_color"));
         mColorPicker.setColor(savedInstanceState.getInt("new_color"), true);
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/havoc/support/colorpicker/ColorPickerPreference.java
@@ -30,8 +30,6 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
@@ -43,6 +41,7 @@ import android.widget.LinearLayout;
 import androidx.preference.*;
 
 import com.havoc.support.R;
+import com.havoc.support.util.VibrationUtils;
 
 /**
  * A preference type that allows a user to choose a time
@@ -71,7 +70,6 @@ public class ColorPickerPreference extends Preference implements
     private EditText mEditText;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     //private boolean mIsCrappyLedDevice;
 
@@ -94,7 +92,6 @@ public class ColorPickerPreference extends Preference implements
         init(context, attrs);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override
@@ -190,7 +187,7 @@ public class ColorPickerPreference extends Preference implements
             @Override
             public void onClick(View v) {
                 onColorChanged(mDefaultValue);
-                doHapticFeedback();
+                VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
             }
         });
         // sorcery for a linear layout ugh
@@ -432,14 +429,5 @@ public class ColorPickerPreference extends Preference implements
         shape.setIntrinsicWidth(size);
         shape.getPaint().setColor(color);
         return shape;
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/havoc/support/preferences/CustomSeekBarPreference.java
@@ -21,8 +21,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -37,6 +35,7 @@ import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.*;
 
 import com.havoc.support.R;
+import com.havoc.support.util.VibrationUtils;
 
 public class CustomSeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener,
         View.OnClickListener, View.OnLongClickListener {
@@ -66,7 +65,6 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     protected int mTrackingValue;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public CustomSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -109,7 +107,6 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         setLayoutResource(R.layout.preference_custom_seekbar);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public CustomSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -225,7 +222,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         if (mTrackingTouch && !mContinuousUpdates) {
             mTrackingValue = newValue;
             updateValueViews();
-            doHapticFeedback(VibrationEffect.EFFECT_TEXTURE_TICK);
+            VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_TEXTURE_TICK);
         } else if (mValue != newValue) {
             if (!callChangeListener(newValue)) {
                 mSeekBar.setProgress(getSeekValue(mValue));
@@ -265,7 +262,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         } else if (id == R.id.plus) {
             setValue(mValue + mInterval, true);
         }
-        doHapticFeedback(VibrationEffect.EFFECT_CLICK);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 
     @Override
@@ -353,14 +350,5 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
 
     public void refresh(int newValue) {
         setValue(newValue, mSeekBar != null);
-    }
-
-    private void doHapticFeedback(int effect) {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(effect));
-        }
     }
 }

src/com/havoc/support/preferences/MasterSwitchPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 Havoc-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +20,6 @@ package com.havoc.support.preferences;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -29,6 +29,7 @@ import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.PreferenceViewHolder;
 
 import com.havoc.support.R;
+import com.havoc.support.util.VibrationUtils;
 
 /**
  * A custom preference that provides inline switch toggle. It has a mandatory field for title, and
@@ -41,14 +42,12 @@ public class MasterSwitchPreference extends TwoTargetPreference {
     private boolean mEnableSwitch = true;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public MasterSwitchPreference(Context context, AttributeSet attrs,
                                   int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public MasterSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -87,7 +86,7 @@ public class MasterSwitchPreference extends TwoTargetPreference {
                     } else {
                         persistBoolean(mChecked);
                     }
-                    doHapticFeedback();
+                    VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
                 }
             });
         }
@@ -155,14 +154,5 @@ public class MasterSwitchPreference extends TwoTargetPreference {
         // Using getString instead of getInt so we can simply check for null
         // instead of catching an exception. (All values are stored as strings.)
         return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/havoc/support/preferences/SwitchPreference.java
@@ -18,25 +18,23 @@ package com.havoc.support.preferences;
 
 import android.content.Context;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.R;
 
+import com.havoc.support.util.VibrationUtils;
+
 public class SwitchPreference extends androidx.preference.SwitchPreference {
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -58,16 +56,6 @@ public class SwitchPreference extends androidx.preference.SwitchPreference {
     @Override
     protected void performClick(View view) {
         super.performClick(view);
-
-        doHapticFeedback();
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 }

src/com/havoc/support/util/VibrationUtils.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.util;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+public class VibrationUtils {
+
+    public static void doHapticFeedback(Context context, int effect) {
+        final Vibrator mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        final boolean hapticEnabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+        if (mVibrator != null && mVibrator.hasVibrator() && hapticEnabled) {
+            mVibrator.vibrate(VibrationEffect.get(effect));
+        }
+    }
+}

PalladiumOS-Temp/platform_vendor_support

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

src/com/palladium/support/colorpicker/ColorPickerDialog.java
@@ -23,8 +23,6 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -36,6 +34,9 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 
 import com.palladium.support.R;
+import com.palladium.support.util.VibrationUtils;
+
+public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 
     private ColorPickerView mColorPicker;
     private ColorPickerPanelView mOldColor;
@@ -45,7 +46,6 @@ import com.palladium.support.R;
     private OnColorChangedListener mListener;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public interface OnColorChangedListener {
         void onColorChanged(int color);
@@ -57,7 +57,6 @@ import com.palladium.support.R;
         init(initialColor);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     private void init(int color) {
@@ -154,7 +153,7 @@ import com.palladium.support.R;
                 mListener.onColorChanged(mNewColor.getColor());
             }
         }
-        doHapticFeedback();
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
         dismiss();
     }
 
@@ -173,14 +172,5 @@ import com.palladium.support.R;
         super.onRestoreInstanceState(savedInstanceState);
         mOldColor.setColor(savedInstanceState.getInt("old_color"));
         mColorPicker.setColor(savedInstanceState.getInt("new_color"), true);
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/palladium/support/colorpicker/ColorPickerPreference.java
@@ -30,8 +30,6 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
@@ -43,6 +41,7 @@ import android.widget.LinearLayout;
 import androidx.preference.*;
 
 import com.palladium.support.R;
+import com.palladium.support.util.VibrationUtils;
 
 /**
  * A preference type that allows a user to choose a time
@@ -71,7 +70,6 @@ public class ColorPickerPreference extends Preference implements
     private EditText mEditText;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     //private boolean mIsCrappyLedDevice;
 
@@ -94,7 +92,6 @@ public class ColorPickerPreference extends Preference implements
         init(context, attrs);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override
@@ -190,7 +187,7 @@ public class ColorPickerPreference extends Preference implements
             @Override
             public void onClick(View v) {
                 onColorChanged(mDefaultValue);
-                doHapticFeedback();
+                VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
             }
         });
         // sorcery for a linear layout ugh
@@ -432,14 +429,5 @@ public class ColorPickerPreference extends Preference implements
         shape.setIntrinsicWidth(size);
         shape.getPaint().setColor(color);
         return shape;
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/palladium/support/preferences/CustomSeekBarPreference.java
@@ -21,8 +21,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -37,6 +35,7 @@ import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.*;
 
 import com.palladium.support.R;
+import com.palladium.support.util.VibrationUtils;
 
 public class CustomSeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener,
         View.OnClickListener, View.OnLongClickListener {
@@ -66,7 +65,6 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     protected int mTrackingValue;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public CustomSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -109,7 +107,6 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         setLayoutResource(R.layout.preference_custom_seekbar);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public CustomSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -225,7 +222,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         if (mTrackingTouch && !mContinuousUpdates) {
             mTrackingValue = newValue;
             updateValueViews();
-            doHapticFeedback(VibrationEffect.EFFECT_TEXTURE_TICK);
+            VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_TEXTURE_TICK);
         } else if (mValue != newValue) {
             if (!callChangeListener(newValue)) {
                 mSeekBar.setProgress(getSeekValue(mValue));
@@ -265,7 +262,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         } else if (id == R.id.plus) {
             setValue(mValue + mInterval, true);
         }
-        doHapticFeedback(VibrationEffect.EFFECT_CLICK);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 
     @Override
@@ -353,14 +350,5 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
 
     public void refresh(int newValue) {
         setValue(newValue, mSeekBar != null);
-    }
-
-    private void doHapticFeedback(int effect) {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(effect));
-        }
     }
 }

src/com/palladium/support/preferences/MasterSwitchPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 Havoc-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +20,6 @@ package com.palladium.support.preferences;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -29,6 +29,7 @@ import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.PreferenceViewHolder;
 
 import com.palladium.support.R;
+import com.palladium.support.util.VibrationUtils;
 
 /**
  * A custom preference that provides inline switch toggle. It has a mandatory field for title, and
@@ -41,14 +42,12 @@ public class MasterSwitchPreference extends TwoTargetPreference {
     private boolean mEnableSwitch = true;
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public MasterSwitchPreference(Context context, AttributeSet attrs,
                                   int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public MasterSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -87,7 +86,7 @@ public class MasterSwitchPreference extends TwoTargetPreference {
                     } else {
                         persistBoolean(mChecked);
                     }
-                    doHapticFeedback();
+                    VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
                 }
             });
         }
@@ -155,14 +154,5 @@ public class MasterSwitchPreference extends TwoTargetPreference {
         // Using getString instead of getInt so we can simply check for null
         // instead of catching an exception. (All values are stored as strings.)
         return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
     }
 }

src/com/palladium/support/preferences/SwitchPreference.java
@@ -18,25 +18,23 @@ package com.palladium.support.preferences;
 
 import android.content.Context;
 import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.R;
 
+import com.palladium.support.util.VibrationUtils;
+
 public class SwitchPreference extends androidx.preference.SwitchPreference {
 
     private final Context mContext;
-    private final Vibrator mVibrator;
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         mContext = context;
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -58,16 +56,6 @@ public class SwitchPreference extends androidx.preference.SwitchPreference {
     @Override
     protected void performClick(View view) {
         super.performClick(view);
-
-        doHapticFeedback();
-    }
-
-    private void doHapticFeedback() {
-        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
-
-        if (hapticEnabled) {
-            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-        }
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
     }
 }

src/com/palladium/support/util/VibrationUtils.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palladium.support.util;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+public class VibrationUtils {
+
+    public static void doHapticFeedback(Context context, int effect) {
+        final Vibrator mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        final boolean hapticEnabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+        if (mVibrator != null && mVibrator.hasVibrator() && hapticEnabled) {
+            mVibrator.vibrate(VibrationEffect.get(effect));
+        }
+    }
+}